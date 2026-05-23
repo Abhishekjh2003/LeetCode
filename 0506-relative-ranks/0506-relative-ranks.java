bcdd468cdd3bb7df5588f4b1d8a22cf76daae995
ABHISHEK HUDED <abhishekhuded506@gmail.com>
2026-05-23 09:50:13 +0530

    Time: 125 ms (5.03%), Space: 47.3 MB (90.88%) - LeetHub

--- 0506-relative-ranks/0506-relative-ranks.java
+++ 0506-relative-ranks/0506-relative-ranks.java
@@ -7,32 +7,67 @@ class Solution {
 
         String[] result = new String[n];
 
-        int[] copy = score.clone();
+        // int[] copy = score.clone();
 
-        Arrays.sort(copy);
+        // Arrays.sort(copy);
 
-        HashMap<Integer, String> map = new HashMap<>();
+        // HashMap<Integer, String> map = new HashMap<>();
 
-        for(int i = n - 1; i >= 0; i--) {
+        // for(int i = n - 1; i >= 0; i--) {
 
-            if(i == n - 1) {
-                map.put(copy[i], "Gold Medal");
+        //     if(i == n - 1) {
+        //         map.put(copy[i], "Gold Medal");
+        //     }
+        //     else if(i == n - 2) {
+        //         map.put(copy[i], "Silver Medal");
+        //     }
+        //     else if(i == n - 3) {
+        //         map.put(copy[i], "Bronze Medal");
+        //     }
+        //     else {
+        //         map.put(copy[i], String.valueOf(n - i));
+        //     }
+        // }
+
+        // for(int i = 0; i < n; i++) {
+        //     result[i] = map.get(score[i]);
+        // }
+
+        // return result;
+
+
+
+
+
+
+
+        for(int i=0;i<n;i++)
+        {
+            int rank =1;
+            for(int  j=0;j<n;j++)
+            {
+                if(score[j]>score[i])
+                {
+                    rank++;
+                }
             }
-            else if(i == n - 2) {
-                map.put(copy[i], "Silver Medal");
+            if(rank ==1)
+            {
+                result[i]="Gold Medal";
             }
-            else if(i == n - 3) {
-                map.put(copy[i], "Bronze Medal");
+            else if(rank==2)
+            {
+                result[i]="Silver Medal";
             }
-            else {
-                map.put(copy[i], String.valueOf(n - i));
+             else if(rank==3)
+            {
+                result[i]="Bronze Medal";
+            }
+            else
+            {
+                result[i]=String.valueOf(rank);
             }
         }
-
-        for(int i = 0; i < n; i++) {
-            result[i] = map.get(score[i]);
-        }
-
         return result;
     }
 }
