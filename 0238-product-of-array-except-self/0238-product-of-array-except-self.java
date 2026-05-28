53fae047cfb64cc45b6fac8098166abf6f345720
ABHISHEK HUDED <abhishekhuded506@gmail.com>
2026-05-28 20:16:08 +0530

    Time: 2 ms (95.27%), Space: 72.4 MB (6.48%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -8,12 +8,15 @@ class Solution {
         {
             res[i]=res[i-1]*nums[i-1];
         }
+
         int right =1;
+        
         for(int i=n-1;i>=0;i--)
         {
             res[i]=res[i]*right;
             right=right*nums[i];
         }
+        
         return res;
     }
 }
