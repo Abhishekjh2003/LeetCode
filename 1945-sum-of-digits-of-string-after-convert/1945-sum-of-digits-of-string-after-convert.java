293612a9ddbb1baf4a0264a5c15e89a29a8d4997
ABHISHEK HUDED <abhishekhuded506@gmail.com>
2026-05-29 18:46:34 +0530

    Time: 5 ms (16.24%), Space: 44.5 MB (10.77%) - LeetHub

--- 1945-sum-of-digits-of-string-after-convert/1945-sum-of-digits-of-string-after-convert.java
+++ 1945-sum-of-digits-of-string-after-convert/1945-sum-of-digits-of-string-after-convert.java
@@ -6,7 +6,6 @@ class Solution {
             int val = s.charAt(i) - 'a' + 1;
             s1 += val;
         }
-
         int ans = 0;
 
         while (k > 0) {
@@ -14,17 +13,14 @@ class Solution {
             s1 = String.valueOf(ans);
             k--;
         }
-
         return ans;
     }
-
     public int sum(String num) {
         int sum = 0;
 
         for (int i = 0; i < num.length(); i++) {
             sum += num.charAt(i) - '0';
         }
-
         return sum;
     }
 }
