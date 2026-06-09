ecfd48f4e9251d228709980ee0b1d46c7f42589d
ABHISHEK HUDED <abhishekhuded506@gmail.com>
2026-06-09 22:04:41 +0530

    Time: 7 ms (68.7%), Space: 47 MB (9.5%) - LeetHub

--- 1314-matrix-block-sum/1314-matrix-block-sum.java
+++ 1314-matrix-block-sum/1314-matrix-block-sum.java
@@ -2,7 +2,6 @@ class Solution {
     public int[][] matrixBlockSum(int[][] mat, int k) {
               int m = mat.length;
         int n = mat[0].length;
-
         int[][] prefix = new int[m + 1][n + 1];
 
         // Build prefix sum
@@ -14,9 +13,7 @@ class Solution {
                         - prefix[i - 1][j - 1];
             }
         }
-
         int[][] ans = new int[m][n];
-
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 int r1 = Math.max(0, i - k);
