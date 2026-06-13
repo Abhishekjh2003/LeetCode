0d13915cae7bd2a5cd1d790ca5c1718e88f368af
ABHISHEK HUDED <abhishekhuded506@gmail.com>
2026-06-13 22:30:34 +0530

    Time: 1 ms (99.9%), Space: 55.7 MB (45.98%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -2,13 +2,11 @@ class Solution {
     public int majorityElement(int[] nums) {
         int count = 0;
         int candidate = 0;
-
         for (int num : nums) {
             // If count reaches 0, we pick a new candidate
             if (count == 0) {
                 candidate = num;
-            }
-            
+            }      
             // If the current number matches the candidate, increment count.
             // Otherwise, decrement count.
             if (num == candidate) {
@@ -17,7 +15,6 @@ class Solution {
                 count--;
             }
         }
-
         return candidate;
     }
 }
