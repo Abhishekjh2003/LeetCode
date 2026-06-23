669f6e98c3766beeeb03c901a4ddd0a2fe52310e
ABHISHEK HUDED <abhishekhuded506@gmail.com>
2026-06-23 22:43:40 +0530

    Time: 431 ms (57.5%), Space: 46.8 MB (37.5%) - LeetHub

---
+++ 3699-number-of-zigzag-arrays-i/3699-number-of-zigzag-arrays-i.java
@@ -0,0 +1,43 @@
+class Solution {
+    static final int MOD = 1_000_000_007;
+
+    public int zigZagArrays(int n, int l, int r) {
+        int m = r - l + 1;
+
+        long[] up = new long[m];
+        long[] down = new long[m];
+
+        // length 2
+        for (int i = 0; i < m; i++) {
+            up[i] = i;          // previous smaller
+            down[i] = m - i - 1; // previous greater
+        }
+
+        for (int len = 3; len <= n; len++) {
+            long[] newUp = new long[m];
+            long[] newDown = new long[m];
+
+            long prefix = 0;
+            for (int i = 0; i < m; i++) {
+                newUp[i] = prefix;
+                prefix = (prefix + down[i]) % MOD;
+            }
+
+            long suffix = 0;
+            for (int i = m - 1; i >= 0; i--) {
+                newDown[i] = suffix;
+                suffix = (suffix + up[i]) % MOD;
+            }
+
+            up = newUp;
+            down = newDown;
+        }
+
+        long ans = 0;
+        for (int i = 0; i < m; i++) {
+            ans = (ans + up[i] + down[i]) % MOD;
+        }
+
+        return (int) ans;
+    }
+}
