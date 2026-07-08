27a6d9e69492f91427ed0a736d529cf35646bceb
ABHISHEK HUDED <abhishekhuded506@gmail.com>
2026-07-08 20:08:58 +0530

    Time: 6 ms (75.26%), Space: 67.1 MB (12.4%) - LeetHub

---
+++ 1749-maximum-absolute-sum-of-any-subarray/1749-maximum-absolute-sum-of-any-subarray.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxAbsoluteSum(int[] nums) {
+        int maxEnding = 0, maxSum = 0;
+        int minEnding = 0, minSum = 0;
+
+        for (int num : nums) {
+            maxEnding = Math.max(0, maxEnding + num);
+            maxSum = Math.max(maxSum, maxEnding);
+
+            minEnding = Math.min(0, minEnding + num);
+            minSum = Math.min(minSum, minEnding);
+        }
+
+        return Math.max(maxSum, Math.abs(minSum));
+    }
+}
