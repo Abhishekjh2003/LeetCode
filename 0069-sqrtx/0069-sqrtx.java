2feef8b6a10206ed931a4ce21f3355f576b91a9f
ABHISHEK HUDED <abhishekhuded506@gmail.com>
2026-07-12 19:38:47 +0530

    Time: 1 ms (97.65%), Space: 42.8 MB (23.29%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,33 +1,23 @@
 class Solution {
     public int mySqrt(int x) {
-            if(x == 0 || x == 1) {
+        if (x < 2) {
             return x;
         }
 
-        int l = 1;
-        int r = x;
+        int left = 1, right = x;
+        int ans = 0;
 
-        while(l <= r) {
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
 
-            int mid = l + (r - l) / 2;
-
-        
-            long square = (long) mid * mid;
-
-            if(square == x) {
-                return mid;
-            }
-
-            else if(square < x) {
-                l = mid + 1;
-            }
-
-            else {
-                r = mid - 1;
+            if (mid <= x / mid) {
+                ans = mid;
+                left = mid + 1;
+            } else {
+                right = mid - 1;
             }
         }
 
-        return r;
-        
+        return ans;
     }
 }
