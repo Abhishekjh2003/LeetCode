deb9ae2c852201bc84bbe4ee54c5fb011c2ceec7
ABHISHEK HUDED <abhishekhuded506@gmail.com>
2026-07-24 18:57:21 +0530

    Time: 1329 ms (27.27%), Space: 47.2 MB (16.36%) - LeetHub

--- 3514-number-of-unique-xor-triplets-ii/3514-number-of-unique-xor-triplets-ii.java
+++ 3514-number-of-unique-xor-triplets-ii/3514-number-of-unique-xor-triplets-ii.java
@@ -3,27 +3,21 @@ import java.util.*;
 class Solution {
     public int uniqueXorTriplets(int[] nums) {
         int n = nums.length;
-
         if (n == 1) return 1;
-
         Set<Integer> pairXor = new HashSet<>();
-
         // Compute all unique XORs of two different elements
         for (int i = 0; i < n; i++) {
             for (int j = i + 1; j < n; j++) {
                 pairXor.add(nums[i] ^ nums[j]);
             }
         }
-
         BitSet seen = new BitSet();
-
         // Form triplets by XORing each pair XOR with every element
         for (int val : pairXor) {
             for (int num : nums) {
                 seen.set(val ^ num);
             }
         }
-
         return seen.cardinality();
     }
 }
