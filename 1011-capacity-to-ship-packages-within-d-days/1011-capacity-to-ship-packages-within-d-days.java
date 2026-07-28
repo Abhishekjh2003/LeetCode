c2bea2205e367c12583e66e163f8f9f8ed8253dc
ABHISHEK HUDED <abhishekhuded506@gmail.com>
2026-07-28 20:14:21 +0530

    Time: 11 ms (67.18%), Space: 50 MB (76.85%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.java
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.java
@@ -2,29 +2,23 @@ class Solution {
     public int shipWithinDays(int[] weights, int days) {
         int low = 0;
         int high = 0;
-
         for (int weight : weights) {
             low = Math.max(low, weight);
             high += weight;
         }
-
         while (low < high) {
             int mid = low + (high - low) / 2;
-
             if (canShip(weights, days, mid)) {
                 high = mid;
             } else {
                 low = mid + 1;
             }
         }
-
         return low;
     }
-
     private boolean canShip(int[] weights, int days, int capacity) {
         int requiredDays = 1;
         int currentLoad = 0;
-
         for (int weight : weights) {
             if (currentLoad + weight > capacity) {
                 requiredDays++;
@@ -33,7 +27,6 @@ class Solution {
                 currentLoad += weight;
             }
         }
-
         return requiredDays <= days;
     }
 }
