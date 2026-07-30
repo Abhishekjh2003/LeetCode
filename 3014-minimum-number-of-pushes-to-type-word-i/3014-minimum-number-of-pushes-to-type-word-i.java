b6500cb90b9aaf2727b13141a82e330b3916856c
ABHISHEK HUDED <abhishekhuded506@gmail.com>
2026-07-30 21:31:35 +0530

    Time: 0 ms (100%), Space: 43.1 MB (79.43%) - LeetHub

--- 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.java
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.java
@@ -1,12 +1,10 @@
 class Solution {
     public int minimumPushes(String word) {
         int n = word.length();
-        int pushes = 0;
-
+        int outputs= 0;
         for (int i = 0; i < n; i++) {
-            pushes += (i / 8) + 1;
+            outputs += (i / 8) + 1;
         }
-
-        return pushes;
+        return outputs;
     }
 }
