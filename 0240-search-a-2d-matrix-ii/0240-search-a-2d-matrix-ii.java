e0af58b6dd8a092242bfc6bef3f20aa058687fba
ABHISHEK HUDED <abhishekhuded506@gmail.com>
2026-08-04 22:05:35 +0530

    Time: 3 ms (88.36%), Space: 48.1 MB (84.89%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
@@ -2,10 +2,8 @@ class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
         int m = matrix.length;
         int n = matrix[0].length;
-
         int row = 0;
         int col = n - 1;
-
         while (row < m && col >= 0) {
             if (matrix[row][col] == target) {
                 return true;
@@ -15,7 +13,6 @@ class Solution {
                 row++;
             }
         }
-
         return false;
     }
 }
