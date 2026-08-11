2fb2224ff44cf83fd512dc739f46f4d657d7e51c
ABHISHEK HUDED <abhishekhuded506@gmail.com>
2026-08-11 21:25:32 +0530

    Time: 21 ms (63.66%), Space: 47.9 MB (77.82%) - LeetHub

--- 0227-basic-calculator-ii/0227-basic-calculator-ii.java
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.java
@@ -1,21 +1,15 @@
 import java.util.*;
-
 class Solution {
     public int calculate(String s) {
         Stack<Integer> stack = new Stack<>();
-
         int num = 0;
         char sign = '+';
-
         for (int i = 0; i < s.length(); i++) {
             char ch = s.charAt(i);
-
             if (Character.isDigit(ch)) {
                 num = num * 10 + (ch - '0');
             }
-
             if ((!Character.isDigit(ch) && ch != ' ') || i == s.length() - 1) {
-
                 if (sign == '+') {
                     stack.push(num);
                 } 
@@ -28,18 +22,14 @@ class Solution {
                 else if (sign == '/') {
                     stack.push(stack.pop() / num);
                 }
-
                 sign = ch;
                 num = 0;
             }
         }
-
         int result = 0;
-
         for (int value : stack) {
             result += value;
         }
-
         return result;
     }
 }
