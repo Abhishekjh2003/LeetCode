fcb513d7f27eca386b33635880cbb7eada54db50
ABHISHEK HUDED <abhishekhuded506@gmail.com>
2026-08-21 22:12:49 +0530

    Time: 176 ms (11.76%), Space: 43.5 MB (78.43%) - LeetHub

---
+++ 3116-kth-smallest-amount-with-single-denomination-combination/3116-kth-smallest-amount-with-single-denomination-combination.java
@@ -0,0 +1,69 @@
+import java.util.*;
+
+class Solution {
+    public long findKthSmallest(int[] coins, int k) {
+        long low = 1;
+        long high = (long) Arrays.stream(coins).min().getAsInt() * k;
+
+        while (low < high) {
+            long mid = low + (high - low) / 2;
+
+            if (count(mid, coins) >= k) {
+                high = mid;
+            } else {
+                low = mid + 1;
+            }
+        }
+
+        return low;
+    }
+
+    private long count(long x, int[] coins) {
+        long result = 0;
+        int n = coins.length;
+
+        for (int mask = 1; mask < (1 << n); mask++) {
+            long lcm = 1;
+            boolean valid = true;
+            int bits = 0;
+
+            for (int i = 0; i < n; i++) {
+                if ((mask & (1 << i)) != 0) {
+                    bits++;
+
+                    lcm = lcm(lcm, coins[i]);
+
+                    if (lcm > x) {
+                        valid = false;
+                        break;
+                    }
+                }
+            }
+
+            if (!valid) continue;
+
+            long add = x / lcm;
+
+            if (bits % 2 == 1) {
+                result += add;
+            } else {
+                result -= add;
+            }
+        }
+
+        return result;
+    }
+
+    private long gcd(long a, long b) {
+        while (b != 0) {
+            long temp = a % b;
+            a = b;
+            b = temp;
+        }
+        return a;
+    }
+
+    private long lcm(long a, long b) {
+        return a / gcd(a, b) * b;
+    }
+}
