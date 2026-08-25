d837cca075462568ec0a72e8ab15fefd95a8bffe
ABHISHEK HUDED <abhishekhuded506@gmail.com>
2026-08-25 22:02:03 +0530

    Time: 5 ms (97.79%), Space: 46.8 MB (86.62%) - LeetHub

--- 1381-design-a-stack-with-increment-operation/1381-design-a-stack-with-increment-operation.java
+++ 1381-design-a-stack-with-increment-operation/1381-design-a-stack-with-increment-operation.java
@@ -1,5 +1,4 @@
 class CustomStack {
-
     int[] stack;
     int top;
     int maxSize;
@@ -9,26 +8,20 @@ class CustomStack {
         stack = new int[maxSize];
         top = -1;
     }
-
     public void push(int x) {
         if (top == maxSize - 1) {
             return;
         }
-
         stack[++top] = x;
     }
-
     public int pop() {
         if (top == -1) {
             return -1;
         }
-
         return stack[top--];
     }
-
     public void increment(int k, int val) {
         int limit = Math.min(k, top + 1);
-
         for (int i = 0; i < limit; i++) {
             stack[i] += val;
         }
