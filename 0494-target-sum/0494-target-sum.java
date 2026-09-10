9b59196ceef52064c29661f05d3c8664aec0b9a0
ABHISHEK HUDED <abhishekhuded506@gmail.com>
2026-09-10 21:29:08 +0530

    Time: 3 ms (89.8%), Space: 43 MB (79.03%) - LeetHub

---
+++ 0494-target-sum/0494-target-sum.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int findTargetSumWays(int[] nums, int target) {
+        
+        int total = 0;
+
+        for (int num : nums) {
+            total += num;
+        }
+
+        // Target is outside possible range
+        if (Math.abs(target) > total) {
+            return 0;
+        }
+
+        // Cannot divide equally
+        if ((total + target) % 2 != 0) {
+            return 0;
+        }
+
+        int sum = (total + target) / 2;
+
+        int[] dp = new int[sum + 1];
+
+        dp[0] = 1;
+
+        for (int num : nums) {
+            for (int j = sum; j >= num; j--) {
+                dp[j] += dp[j - num];
+            }
+        }
+
+        return dp[sum];
+    }
+}
